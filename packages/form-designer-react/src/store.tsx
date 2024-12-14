0be8fa8149fade879e373b43e73f20a47a62758f
落雁沙 <fengxiaotx@163.com>
2024-12-14 16:41:02 +0800

    feat: new version

--- packages/form-designer-react/src/store.tsx
+++ packages/form-designer-react/src/store.tsx
@@ -16,7 +16,7 @@ export namespace GlobalStore {
   }
 
   export function getIcon(iconName?: string) {
-    return iconName ?? DESIGNER_ICONS_STORE[iconName];
+    return iconName?DESIGNER_ICONS_STORE[iconName]:null;
   }
 
   //endregion
