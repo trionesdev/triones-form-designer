254e5a5e782655a616a15797cbe5f8d7ae72b136
bane.shi <fengxiaotx@163.com>
2024-12-27 23:55:35 +0800

    fix: onChange trigger late

--- packages/form-designer-react/src/widget/ComponentsWidget/TreeNodeWidget.tsx
+++ packages/form-designer-react/src/widget/ComponentsWidget/TreeNodeWidget.tsx
@@ -17,10 +17,8 @@ export const TreeNodeWidget: FC<ComponentWidgetProps> = observer(
       const Component = components?.[treeNode.componentName];
 
       const renderChildren = () => {
-        debugger;
         if (treeNode.children.length > 0) {
           return treeNode.children.map((item, index) => {
-            debugger;
             return <TreeNodeWidget key={index} treeNode={item} />;
           });
         } else {
